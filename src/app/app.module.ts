81fd7dcb1a33c1d83246d6d09e1098a6571a0cc3
hermbit <hermitsmail@gmail.com>
2018-01-04 17:57:46 -0500

    init core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -15,6 +17,7 @@ import { SignupComponent } from './signup/signup.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
