fbb873b2127fb6e71b0542d41e65a2cb624712a8
hermbit <hermitsmail@gmail.com>
2018-01-04 18:17:47 -0500

    user authentication for sign-up

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AuthService } from './auth.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [AuthService]
 })
 export class CoreModule { }
---
+++ src/app/core/user.interface.ts
@@ -0,0 +1,15 @@
+export interface User {
+  uid: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  country: string;
+  company: string;
+  licenseId: string;
+  avatarUrl: string;
+  accountType: 'agent' | 'manager';
+}
