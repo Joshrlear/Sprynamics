7fddd3ee7c14989272e7c8f2a09a4a8d88362f0c
hermbit <hermitsmail@gmail.com>
2018-01-04 18:18:42 -0500

    add signup route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SignupComponent } from './signup/signup.component';
+
 const routes: Routes = [
-  {}
+  { path: 'login', component: SignupComponent },
 ];
 
 @NgModule({
