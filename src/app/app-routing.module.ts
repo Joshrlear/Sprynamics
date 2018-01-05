2e6cfe7f247c346a5b6a70097ba3f5a2f6a3594c
hermbit <hermitsmail@gmail.com>
2018-01-04 23:52:43 -0500

    build authentication and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
+import { AuthGuard } from './core/auth.guard';
+
 const routes: Routes = [
-  { path: 'login', component: SignupComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [ AuthGuard ]
 })
 export class AppRoutingModule { }
--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
   constructor(private afAuth: AngularFireAuth,
               private afs: AngularFirestore,
               private router: Router) {
-    
+
     // Get auth data, then get firestore user document || null
     this.user = this.afAuth.authState
       .switchMap(user => {
@@ -47,6 +47,10 @@ export class AuthService {
   // update properties on user document
   updateUserData(user: User, data: Partial<User>) {
     return this.afs.doc<User>(`users/${user.uid}`).update(data);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
