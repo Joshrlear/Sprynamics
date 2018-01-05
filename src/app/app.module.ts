ffc45d95b3dc98480b0229dfafa4085a17b984d0
hermbit <hermitsmail@gmail.com>
2018-01-05 02:42:02 -0500

    add designer route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { DesignerComponent } from './designer/designer.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HomeComponent } from './home/home.component';
     LoginComponent,
     DashboardComponent,
     HomeComponent,
+    DesignerComponent,
   ],
   imports: [
     BrowserModule,
