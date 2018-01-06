6d6f922f05ed4a4e8554e2ce464bf3b637ac6aec
hermbit <hermitsmail@gmail.com>
2018-01-05 22:01:37 -0500

    create storage service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,7 +4,9 @@ import { environment } from '../../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AuthService } from './auth.service';
+import { StorageService } from './storage.service';
 
 @NgModule({
   imports: [
@@ -13,6 +15,6 @@ import { AuthService } from './auth.service';
     AngularFirestoreModule
   ],
   declarations: [],
-  providers: [AuthService]
+  providers: [AuthService, StorageService]
 })
 export class CoreModule { }
