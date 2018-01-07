af7cfe321febad2ad3729828dd9287478a2b2978
Royce <royceshayegan@gmail.com>
2018-01-07 03:54:20 -0500

    built home page - hero section added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from '@angular/core';
 
-import { FirebaseApp } from '@firebase/app-types';
-import { FirebaseAuth } from '@firebase/auth-types';
-import { FirebaseDatabase } from '@firebase/database-types';
-import { FirebaseMessaging } from '@firebase/messaging-types';
-import { FirebaseStorage } from '@firebase/storage-types';
-import { FirebaseFirestore } from '@firebase/firestore-types';
 import { environment } from '../../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
