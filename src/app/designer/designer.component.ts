10c677f93369e55cdf4e6f1a1a5280f9094a6d1e
hermbit <hermitsmail@gmail.com>
2018-01-09 01:28:43 -0500

    first steps of template designer

--- src/app/designer/designer.component.ts
+++ src/app/designer/designer.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import 'fabric';
+declare let fabric;
 
 @Component({
   selector: 'app-designer',
   templateUrl: './designer.component.html',
   styleUrls: ['./designer.component.css']
 })
-export class DesignerComponent implements OnInit {
+export class DesignerComponent implements OnInit, AfterViewInit {
+  
+  @ViewChild('designerView') view: ElementRef;
 
-  constructor() { }
+  canvas;
+  boundBox;
+  shape;
+
+  constructor(private element: ElementRef) { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
+    this.canvas = new fabric.Canvas('canvas', {
+      width: this.view.nativeElement.clientWidth,
+      height: this.view.nativeElement.clientHeight
+    });
+
+    this.boundBox = new fabric.Rect({
+      width: 500,
+      height: 600,
+      fill: 'transparent',
+      stroke: '#777',
+      strokeDashArray: [5,5]
+    });
+
+    this.shape = new fabric.Rect({
+      width: 200,
+      height: 200,
+      fill: 'red',
+    });
+
+    this.canvas.add(this.boundBox);
+    this.canvas.add(this.shape);
+
+    this.canvas.centerObject(this.boundBox);
+  }
+
+  addText() {
+
+  }
+
+  addShape() {
+    const shape = new fabric.Rect({
+      width: 200,
+      height: 200,
+      fill: 'red',
+    });
+
+    this.canvas.add(shape);
+  }
+
+  addImage() {
+
   }
 
 }
