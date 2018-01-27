6ec6a7c1bf0278c19a26712f7fd9f1620524585e
hermbit <hermitsmail@gmail.com>
2018-01-27 04:30:29 -0500

    fix mobile page links (fixes #96)

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
+import { Router } from '@angular/router';
+
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'navigation',
@@ -8,16 +12,35 @@ import { AuthService } from '../core/auth.service';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, private router: Router) { }
 
   ngOnInit() {
 
   }
 
+  ngAfterViewInit() {
+
+  }
+
+  smoothScroll(hash) {
+    if (this.router.url.split('#')[0] === '/') {
+      hash = '#' + hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+  
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        if (hash !== '#home') {
+          window.location.hash = hash;
+        }
+      });
+    }
+  }
+
   toggleMenu() {
     document.getElementById("mobile-menu_dropdown").classList.toggle("active");
   }
-  
+
   public logout() {
     this.auth.logout();
   }
