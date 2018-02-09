5ba2f0c65681c209ee506f1e9113efc7308733a5
Royce <royceshayegan@gmail.com>
2018-02-09 12:10:28 -0500

    checkout initial commit

---
+++ src/app/checkout/order-summary/order-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'order-summary',
+  templateUrl: './order-summary.component.html',
+  styleUrls: ['./order-summary.component.scss']
+})
+export class OrderSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
