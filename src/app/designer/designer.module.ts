b97a0555f9c7846520c313d24cd05494aa7acb6c
hermbit <hermitsmail@gmail.com>
2018-05-28 07:11:32 -0400

    refactor designer view

--- src/app/designer/designer.module.ts
+++ src/app/designer/designer.module.ts
@@ -16,9 +16,9 @@ import { ImageSelectDialog } from './image-select-dialog/image-select.dialog';
 import { AdminDesignerProgressDialog } from './admin/admin-designer-progress-dialog/admin-designer-progress.dialog';
 import { DesignerDevComponent } from './dev/designer-dev.component';
 import { ProductsComponent } from './sidebar/products/products.component';
-import { DesignerViewComponent } from '#app/designer/designer-view/designer-view.component';
+import { DesignerViewComponent } from '#app/designer/view/designer-view.component';
 import { FabricCanvasComponent } from '#app/designer/fabric-canvas.component';
-import { SidebarTabComponent } from '#app/designer/designer-view/sidebar-tab.component';
+import { SidebarTabComponent } from '#app/designer/view/sidebar-tab.component';
 
 const routes: Routes = [
   { path: '', component: DesignerDevComponent },
